67f975e2724e35a69ebb819a3c769f5b9121f800
Sathvika3 <88889722+Sathvika3@users.noreply.github.com>
2022-05-11 20:48:28 +0530

    Create ReversePairs.java

---
+++ ReversePairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reversePairs(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]>2*nums[j])
+                    count++;
+            }
+        }
+        return count;
+    }
+        
+    }
